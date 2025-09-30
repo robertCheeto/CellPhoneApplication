1e1633596814c5cab5bbd71c5fcc408d18e9f85d
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-30 15:17:52 -0400

    * created private variables in CellPhone Class * created getter() for serialNumber * created parameterless Constructor * created CellPhone object in CellPhoneApplication

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
 
 public class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+}
+
+public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+    this.serialNumber = serialNumber;
+    this.model = model;
+    this.carrier = carrier;
+    this.phoneNumber = phoneNumber;
+    this.owner = owner;
+}
+
+public CellPhone() {
+    this.serialNumber = 0;
+}
+
+public int getSerialNumber() {
+    return this.serialNumber;
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -2,6 +2,8 @@ package com.pluralsight;
 
 public class CellPhoneApplication {
     public static void main(String[] args) {
+        CellPhone phone = new CellPhone(123456789, "iPhone 17", "Xfinity Mobile", "412-999-6789", "Daniel Spezialetti");
+
 
     }
 }
