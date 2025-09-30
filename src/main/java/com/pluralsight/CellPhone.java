5a8f15b18e999811226754026700ca311ba51a16
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-30 15:29:34 -0400

    * adjusted CellPhone() curly braces so other methods exist inside as main blocks * fixed setSerialNumber() to no longer be a return method

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -6,32 +6,33 @@ public class CellPhone {
     private String carrier;
     private String phoneNumber;
     private String owner;
-}
 
-public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
-    this.serialNumber = serialNumber;
-    this.model = model;
-    this.carrier = carrier;
-    this.phoneNumber = phoneNumber;
-    this.owner = owner;
-}
 
-public CellPhone() {
-    this.serialNumber = 0;
-    this.model = "";
-    this.carrier = "";
-    this.phoneNumber = "";
-    this.owner = "";
-}
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner) {
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+    }
 
-public int getSerialNumber() {
-    return this.serialNumber;
-}
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
 
-public int setSerialNumber(int serialNumber) {
-    this.serialNumber = serialNumber;
-}
+    public int getSerialNumber() {
+        return this.serialNumber;
+    }
 
-public void phoneInformation() {
-    System.out.printf("%s is the owner of the %s with the Serial Number of %d", owner, model, serialNumber);
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void phoneInformation() {
+        System.out.printf("%s is the owner of the %s with the Serial Number of %d", owner, model, serialNumber);
+    }
 }
