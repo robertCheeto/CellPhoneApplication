001cb2a505b22f6a67465778124a9240281ac3ac
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-30 15:24:37 -0400

    * created setter for serialNumber * created a phoneInformation() for phone information to be output

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -18,8 +18,20 @@ public CellPhone(int serialNumber, String model, String carrier, String phoneNum
 
 public CellPhone() {
     this.serialNumber = 0;
+    this.model = "";
+    this.carrier = "";
+    this.phoneNumber = "";
+    this.owner = "";
 }
 
 public int getSerialNumber() {
     return this.serialNumber;
+}
+
+public int setSerialNumber(int serialNumber) {
+    this.serialNumber = serialNumber;
+}
+
+public void phoneInformation() {
+    System.out.printf("%s is the owner of the %s with the Serial Number of %d", owner, model, serialNumber);
 }
