71b0ebeac7b1c9280763933122b3a8a5a9ff1d4e
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-09-30 15:35:57 -0400

    * added remaining getters and setters for other data types

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -14,7 +14,7 @@ public class CellPhone {
         this.carrier = carrier;
         this.phoneNumber = phoneNumber;
         this.owner = owner;
-    }
+    } // CellPhone Constructor
 
     public CellPhone() {
         this.serialNumber = 0;
@@ -22,15 +22,47 @@ public class CellPhone {
         this.carrier = "";
         this.phoneNumber = "";
         this.owner = "";
-    }
+    } // Parameterless Constructor | ask Topher what this does/means for better clarity
 
     public int getSerialNumber() {
         return this.serialNumber;
-    }
+    } // Getter for serialNumber()
 
     public void setSerialNumber(int serialNumber) {
         this.serialNumber = serialNumber;
-    }
+    } // Setter for serialNumber()
+
+    public String getModel(String model) {
+        return this.model;
+    } // Getter for model()
+
+    public void setModel() {
+        this.model = model;
+    } // Setter for model();
+
+    public String getCarrier() {
+        return this.carrier;
+    } // Getter for carrier()
+
+    public void setCarrier() {
+        this.carrier = carrier;
+    } // Setter for carrier()
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    } // Getter for phoneNumber()
+
+    public void setPhoneNumber() {
+        this.phoneNumber = phoneNumber;
+    } // Setter for phoneNumber()
+
+    public String getOwner() {
+        return this.owner;
+    } // Getter for owner()
+
+    public void setOwner() {
+        this.owner = owner;
+    } // Setter for owner()
 
     public void phoneInformation() {
         System.out.printf("%s is the owner of the %s with the Serial Number of %d", owner, model, serialNumber);
